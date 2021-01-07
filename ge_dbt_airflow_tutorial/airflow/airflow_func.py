c0fb71ad8f583e0303cfd24564a40267aa05c803
y_tee <thisisytee@gmail.com>
2021-01-07 22:19:56 +0800

    added more to airflow_func.py

--- ge_dbt_airflow_tutorial/airflow/airflow_func.py
+++ ge_dbt_airflow_tutorial/airflow/airflow_func.py
@@ -8,7 +8,21 @@ class moneylion_redshift:
     def __init__(self, ymlfilelocation):
         self.context = ge.data_context.DataContext(ymlfilelocation)
 
-    def create_expectation_from_table_defined(self, schema, table, datecolumn, startdate, enddate,unique_column):
+    def create_expectation_from_table_defined(
+        self,
+        expectation_name,
+        schema,
+        table,
+        datecolumn,
+        startdate,
+        enddate,
+        unique_column1=None,
+        unique_column2=None,
+        unique_column3=None,
+        columnA=None,
+        columnB=None,
+        columntype=None,
+    ):
         batch_kwargs = self.context.build_batch_kwargs(
             "my_redshift_db",
             "queries",
@@ -18,24 +32,55 @@ class moneylion_redshift:
                 "table": table,
                 "datecolumn": datecolumn,
                 "start": startdate,
-                "end": enddate
-            }
+                "end": enddate,
+            },
         )
 
         suite = self.context.create_expectation_suite(
-            "my_expectation_suite", overwrite_existing=True
+            expectation_name, overwrite_existing=True
         )
 
         batch = self.context.get_batch(
-            batch_kwargs=batch_kwargs,
-            expectation_suite_name=suite
+            batch_kwargs=batch_kwargs, expectation_suite_name=suite
         )
 
-        batch.expect_column_values_to_be_unique(unique_column)
+        try:
+            batch.expect_table_row_count_to_be_between(min_value=100, max_value=500000)
+        except Exception as e:
+            print(e)
+            print("expected table row count wasnt given")
+
+        try:
+            batch.expect_column_values_to_be_of_type(columnA, columntype)
+        except Exception as e:
+            print(e)
+            print("expected column type wasnt given")
+
+        try:
+            batch.expect_column_values_to_be_unique(unique_column1)
+        except Exception as e:
+            print(e)
+            print("column to be unique is not provided")
+
+        try:
+            batch.expect_compound_columns_to_be_unique(
+                [unique_column1, unique_column2, unique_column3]
+            )
+        except Exception as e:
+            print(e)
+            print("compound column to be unique is not provided")
+
+        try:
+            batch.expect_column_pair_values_to_be_equal(columnA, columnB)
+        except Exception as e:
+            print(e)
+            print("equal column pair is not provided")
 
         batch.save_expectation_suite()
 
-    def create_expectation_from_table_profiled(self, schema, table, datecolumn, startdate, enddate):
+    def create_expectation_from_table_profiled(
+        self, schema, table, datecolumn, startdate, enddate
+    ):
         batch_kwargs = self.context.build_batch_kwargs(
             "my_redshift_db",
             "queries",
@@ -45,24 +90,28 @@ class moneylion_redshift:
                 "table": table,
                 "datecolumn": datecolumn,
                 "start": startdate,
-                "end": enddate
-            }
+                "end": enddate,
+            },
         )
-        suite_name="profiled_expectation_suite"
+        suite_name = "profiled_expectation_suite"
 
-        suite = self.context.create_expectation_suite(suite_name, overwrite_existing=True)
+        suite = self.context.create_expectation_suite(
+            suite_name, overwrite_existing=True
+        )
 
         batch = self.context.get_batch(
-            batch_kwargs=batch_kwargs,
-            expectation_suite_name=suite
+            batch_kwargs=batch_kwargs, expectation_suite_name=suite
         )
 
-        suite, validation_result = BasicSuiteBuilderProfiler().profile(batch, profiler_configuration="demo")
+        suite, validation_result = BasicSuiteBuilderProfiler().profile(
+            batch, profiler_configuration="demo"
+        )
 
-        self.context.save_expectation_suite(suite,suite_name)
+        self.context.save_expectation_suite(suite, suite_name)
 
-
-    def get_rs_data_to_validate(self, schema, table, datecolumn, startdate, enddate, expectation_suite_name):
+    def get_rs_data_to_validate(
+        self, schema, table, datecolumn, startdate, enddate, expectation_suite_name
+    ):
         batch_kwargs = self.context.build_batch_kwargs(
             "my_redshift_db",
             "queries",
@@ -72,16 +121,19 @@ class moneylion_redshift:
                 "table": table,
                 "datecolumn": datecolumn,
                 "start": startdate,
-                "end": enddate
-            }
+                "end": enddate,
+            },
         )
 
         batch = self.context.get_batch(batch_kwargs, expectation_suite_name)
 
         results = self.context.run_validation_operator(
-        "action_list_operator",
-        assets_to_validate=[batch],
-        run_id="my_run_id")  # e.g., Airflow run id or some run identifier that your pipeline uses.
+            "action_list_operator", assets_to_validate=[batch], run_id="my_run_id"
+        )  # e.g., Airflow run id or some run identifier that your pipeline uses.
 
         if not results["success"]:
-            raise AirflowException("The analytical output does not meet the expectations in the suite: {0:s}".format(expectation_suite_name))
+            raise AirflowException(
+                "The analytical output does not meet the expectations in the suite: {0:s}".format(
+                    expectation_suite_name
+                )
+            )
--- ge_dbt_airflow_tutorial/airflow/ge_dag.py
+++ ge_dbt_airflow_tutorial/airflow/ge_dag.py
@@ -10,22 +10,20 @@ from sqlalchemy import create_engine
 import great_expectations as ge
 from airflow_func import moneylion_redshift
 
-GE_TUTORIAL_DB_URL = os.getenv('GE_TUTORIAL_DB_URL')
-GE_TUTORIAL_ROOT_PATH = os.getenv('GE_TUTORIAL_ROOT_PATH')
+GE_TUTORIAL_DB_URL = os.getenv("GE_TUTORIAL_DB_URL")
+GE_TUTORIAL_ROOT_PATH = os.getenv("GE_TUTORIAL_ROOT_PATH")
 
-great_expectations_context_path = os.getenv('GE_TUTORIAL_GE_CONTEXT_PATH') or os.path.join(GE_TUTORIAL_ROOT_PATH,
-                                                                                           "great_expectations_projects",
-                                                                                           "final",
-                                                                                           "great_expectations")
+great_expectations_context_path = os.getenv(
+    "GE_TUTORIAL_GE_CONTEXT_PATH"
+) or os.path.join(
+    GE_TUTORIAL_ROOT_PATH, "great_expectations_projects", "final", "great_expectations"
+)
 
-default_args = {
-    "owner": "Airflow",
-    "start_date": airflow.utils.dates.days_ago(1)
-}
+default_args = {"owner": "Airflow", "start_date": airflow.utils.dates.days_ago(1)}
 
 # The DAG definition
 dag = DAG(
-    dag_id='my_ge_on_airflow',
+    dag_id="my_ge_on_airflow",
     default_args=default_args,
     schedule_interval=None,
 )
@@ -33,38 +31,53 @@ dag = DAG(
 
 def generate_expectation(ds, **kwargs):
     mr = moneylion_redshift(great_expectations_context_path)
-    mr.create_expectation_from_table_defined("usr_yloo", "transaction_test", "created", '2020-10-26',
-                                             '2020-11-10', 'transaction_id')
+    mr.create_expectation_from_table_defined(
+        "column_type_check",
+        "usr_yloo",
+        "transaction_test",
+        "created",
+        "2020-10-26",
+        "2020-11-10",
+        columnA="created",
+        columntype="TIMESTAMP",
+    )
 
 
 def generate_profile(ds, **kwargs):
     mr = moneylion_redshift(great_expectations_context_path)
-    mr.create_expectation_from_table_profiled("usr_yloo", "transaction_test", "created", '2020-10-26',
-                                              '2020-12-30')
+    mr.create_expectation_from_table_profiled(
+        "usr_yloo", "transaction_test", "created", "2020-10-26", "2020-12-30"
+    )
 
 
 def validate(ds, **kwargs):
     mr = moneylion_redshift(great_expectations_context_path)
-    mr.get_rs_data_to_validate("usr_yloo", "transaction_test", "created", '2020-11-11',
-                               '2020-11-30', 'profiled_expectation_suite')
+    mr.get_rs_data_to_validate(
+        "usr_yloo",
+        "transaction_test",
+        "created",
+        "2020-11-11",
+        "2020-11-30",
+        "column_type_check",
+    )
 
 
 task_generate_exp = PythonOperator(
-    task_id='generate_expectation',
+    task_id="generate_expectation",
     python_callable=generate_expectation,
     provide_context=True,
-    dag=dag)
+    dag=dag,
+)
 
 task_generate_profile = PythonOperator(
-    task_id='generate_profile',
+    task_id="generate_profile",
     python_callable=generate_profile,
     provide_context=True,
-    dag=dag)
+    dag=dag,
+)
 
 validate_data = PythonOperator(
-    task_id='validate_data',
-    python_callable=validate,
-    provide_context=True,
-    dag=dag)
+    task_id="validate_data", python_callable=validate, provide_context=True, dag=dag
+)
 
 task_generate_exp >> task_generate_profile >> validate_data
